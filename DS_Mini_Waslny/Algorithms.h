771b5be0d86597c2fdb2cd95881c7e0ab3cf5979
mayar <mayar@MAYAR>
2023-05-12 18:42:27 +0300

    all graphs are passed by reference

--- DS_Mini_Waslny/Algorithms.h
+++ DS_Mini_Waslny/Algorithms.h
@@ -2,5 +2,5 @@
 #include "graph.h"
 
 void DFS(graph&, string stratVetrex);
-void BFS(string startCity, graph graph);
+void BFS(string startCity, graph& graph);
 float Dijkstra(graph&, string, string, vector<string>&,vector<float>&);
