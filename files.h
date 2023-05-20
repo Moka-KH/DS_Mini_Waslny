1d8cc517332b2127d26ced540769a92aadca15e5
MahmoudHany908 <2021170936@cis.asu.edu.eg>
2023-05-21 00:43:02 +0300

    Added file functionalities(files.h+files.cpp)

---
+++ files.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <QFile>
+#include <QTextStream>
+#include <QMap>
+#include "graph.h"
+
+void writeGraphToFile(graph& g, const QString& filename);
+void readGraphFromFile(graph& g, const QString& filename);
+QMap<QString, graph> readMultipleGraphs(const QString& directory);
+void writeMultipleGraphs(QMap<QString, graph>& graphs, const QString& directory);
