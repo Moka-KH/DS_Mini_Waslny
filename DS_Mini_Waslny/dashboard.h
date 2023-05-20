308604f2c7756503ae7d47acef7e8528e8f7e52d
Ahmed Aboalesaad <53487714+Ahmed-Aboalasaad@users.noreply.github.com>
2023-05-20 15:25:38 +0300

    cleaning the branch

--- DS_Mini_Waslny/dashboard.h
+++
@@ -1,9 +0,0 @@
-#include <stack>
-#include "graph.h"
-
-#pragma once
-
-void updateMap(graph& myGraph);
-void Traverse(graph& mygraph);
-void pathFinder(graph& myMap);
-void displayPath(stack<pair<string, float>> path, string startingCity);
--- DS_Mini_Waslny/files.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<fstream>
-#include"graph.h"
-
-void writeGraphToFile(graph& g, const string& filename);
-void readGraphFromFile(graph& g, const string& filename);
-unordered_map<string, graph> readMultipleGraphs(const string& directory);
-void writeMultipleGraphs( unordered_map<string, graph>& graphs, const string& directory);
